ce1bfc19d01e7d5e9c4f35e3b173b6523cb12996
Subiniiie <subin57419@gmail.com>
2024-10-20 16:30:08 +0900

    프로젝트 구조 수정

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -5,6 +5,13 @@ export default function Project() {
   return (
     <div className="project-container">
         <h1 className="project-title-text">PROJECT</h1>
+        <div className="project-radio-box-container">
+            <p className="project-radio-box-text">주요 프로젝트만 모아보기</p>
+            <input 
+                type='radio'
+                className="project-radio-box-input"
+            />
+        </div>
         <ProjectItems />
     </div>
   )
