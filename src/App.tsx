ed0d2502154c38591e7eb9ca7309139cec39b9a3
Subiniiie <subin57419@gmail.com>
2024-10-29 00:41:56 +0900

    스크롤바 커스텀

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,30 @@ import Skills from './components/skills/Skills'
 import Project from './components/project/Project.tsx'
 import Career from './components/career/Career.tsx'
 import Footer from './components/footer/Footer.tsx'
+import { createGlobalStyle } from 'styled-components'
 import './App.css'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    &::-webkit-scrollbar {
+      width: 8px;
+      height: 8px;
+    }
+    &::-webkit-scrollbar-thumb {
+      // background: rgba(0, 0, 0, 1);
+      border-radius: 6px;
+    }
+    &::-webkit-scrollbar-track {
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 6px;
+    }
+  }
+`
 function App() {
-
 
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Main />
       <AboutMe />
