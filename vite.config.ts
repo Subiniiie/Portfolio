6e3a65a6e978db8997877fd1abb668d2d56f4f6f
박수빈 <subin57419@gmail.com>
2024-11-18 01:24:00 +0900

    build 폴더 생성

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // 출력 디렉토리를 'build'로 변경
+  },
 })
