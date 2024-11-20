30a6056701dc88d6c9d2cf9944ebb12d023c840a
박수빈 <subin57419@gmail.com>
2024-11-20 15:52:41 +0900

    router 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Header from './components/header/Header'
-import MainView from './pages/mainView.tsx'
 import Main from './components/main/Main.tsx'
 import AboutMe from './components/aboutMe/AboutMe.tsx'
 import Career from './components/career/Career.tsx'
@@ -8,7 +7,6 @@ import Project from './components/project/Project.tsx'
 import Footer from './components/footer/Footer.tsx'
 import { createGlobalStyle } from 'styled-components'
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -32,13 +30,11 @@ function App() {
     <>
       <GlobalStyle />
       <Header />
-      <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/aboutme" element={<AboutMe />} />
-            <Route path="/career" element={<Career />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/project" element={<Project />} />
-      </Routes>        
+      <Main />
+      <AboutMe />
+      <Career />
+      <Skills />
+      <Project />
       <Footer />
     </>
   )
