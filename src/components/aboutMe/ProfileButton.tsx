82e102aefd5ef152ec22ad4092fa4a8a3abbf872
Subiniiie <subin57419@gmail.com>
2024-12-01 15:52:56 +0900

    사이트 링크 올리기

--- src/components/aboutMe/ProfileButton.tsx
+++ src/components/aboutMe/ProfileButton.tsx
@@ -4,25 +4,20 @@ const buttons: string[] = ['깃허브', '티스토리'];
 const webSiteUrl: string[] = ['https://github.com/Subiniiie', 'https://subiniiie.tistory.com/']
 
 export default function ProfileButton() {
-  const goMyWebSite = function(index: number) {
-    window.open(webSiteUrl[index], '_blank');
-  };
 
   return (
     <div className="btn-container">
       {buttons.map((button, index) => (
-        <div
-          key={index}
-          onClick={() => goMyWebSite(index)}
-          style={{
-            backgroundColor: 'white',
-            borderColor: "white",
-            color: 'gray',              
-            fontSize: '13px',
-            cursor: "pointer" 
-          }}
-        >
-          {button}
+        <div key={index}>
+          <span>{button} : </span>
+          <span>
+            <a 
+              href={webSiteUrl[index]}
+              target="_blank"
+            >
+              {webSiteUrl[index]}
+            </a>
+          </span>
         </div>
       ))}
     </div>
